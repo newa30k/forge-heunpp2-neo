d132ef85f30f4d6c024c076b966eaf931729fb00
newa30k <newai30k@gmail.com>
2026-09-04 17:04:11 -0300

    Create heunpp2.py

---
+++ scripts/heunpp2.py
@@ -0,0 +1,13 @@
+from modules import sd_samplers_kdiffusion, sd_samplers_common
+from ldm_patched.k_diffusion import sampling as k_diffusion_sampling
+
+class HeunPP2Sampler(sd_samplers_kdiffusion.KDiffusionSampler):
+    def __init__(self, sd_model):
+        super().__init__(k_diffusion_sampling.sample_heunpp2, sd_model, None)
+
+def build_constructor(model):
+    return HeunPP2Sampler(model)
+
+samplers_data_heunpp2 = [
+    sd_samplers_common.SamplerData('HeunPP2', build_constructor, ['heunpp2'], {}),
+]
